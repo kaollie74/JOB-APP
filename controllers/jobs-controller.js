47c2e172bd68f01479da0ba23b2c71fc99bb2021
Kyle Olson <kaollie74@gmail.com>
2022-06-28 20:22:41 -0500

    UPDATE & DELETE job functionality complete.

--- controllers/jobs-controller.js
+++ controllers/jobs-controller.js
@@ -3,16 +3,14 @@ const { NotFoundError, BadRequestError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Jobs.find({ createdBy: req.user.userId }).sort(
-    'createdAt'
-  );
+  const jobs = await Jobs.find({ createdBy: req.user.userId }).sort('createdAt');
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
   const {
     user: { userId },
-    params: { id: jobId },
+    params: { id: jobId }
   } = req;
 
   const job = await Jobs.find({ _id: jobId, createdBy: userId });
@@ -39,7 +37,7 @@ const updateJob = async (req, res) => {
   const {
     user: { userId },
     params: { id: jobId },
-    body: { company, position },
+    body: { company, position }
   } = req;
 
   if (!company || !position) {
@@ -52,14 +50,10 @@ const updateJob = async (req, res) => {
    *   @param 3 {options}
    */
 
-  const job = await Jobs.findByIdAndUpdate(
-    { _id: jobId, createdBy: userId },
-    req.body,
-    {
-      new: true,
-      runValidators: true,
-    }
-  );
+  const job = await Jobs.findByIdAndUpdate({ _id: jobId, createdBy: userId }, req.body, {
+    new: true,
+    runValidators: true
+  });
 
   // throw error if not found
   if (!job) {
@@ -70,13 +64,27 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('deleteJob');
-};
+  const {
+    user: { userId },
+    params: { id: jobId }
+  } = req;
+
+  const job = await Jobs.findOneAndRemove({
+    _id: jobId,
+    createdBy: userId
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).send();
+}; // end deleteJob
 
 module.exports = {
   getAllJobs,
   getJob,
   createJob,
   updateJob,
-  deleteJob,
+  deleteJob
 };
